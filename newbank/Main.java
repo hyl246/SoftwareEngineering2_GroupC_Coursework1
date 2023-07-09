e1a49716f8cf4f8ac0fb16eb33a6a49fa9220d4e
Karan Manhas <karan.manhas@outlook.com>
2023-07-09 17:14:57 +0100

    edited Main.java

--- newbank/Main.java
+++ newbank/Main.java
@@ -1,5 +1,21 @@
 
+//Import Packages
+import newbank.server.*;
+import newbank.client.*;
 
-public static void main(String[] args) {
-    
+public class Main {
+    public static void main(String[] args) {
+        // Use classes from the server subfolder
+        Account account = new Account();
+        Customer customer = new Customer();
+        CustomerID customerID = new CustomerID();
+        NewBank newBank = new NewBank();
+        NewBankClientHandler clientHandler = new NewBankClientHandler();
+        NewBankServer server = new NewBankServer();
+
+        // Use classes from the client subfolder
+        ExampleClient exampleClient = new ExampleClient();
+
+        // Rest of your code
+    }
 }
